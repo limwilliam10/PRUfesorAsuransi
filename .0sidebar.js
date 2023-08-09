3abc0b4c440396c391c4ec92337a8d3a74124126
limwilliam10 <80199957+limwilliam10@users.noreply.github.com>
2023-08-09 18:23:38 +0700

    deployed

---
+++ .0sidebar.js
@@ -0,0 +1,74 @@
+let appSidebar = `
+    <nav>
+
+
+
+    
+
+    <div class="sidebar">
+                
+    <h3 class="sidebar-title">Articles</h3>
+    <strong class="sidebar-subtitle">Edukasi Asuransi</strong>
+    
+    <div class="sidebar-item categories">
+        <ul id="sidebar-nav">
+            
+            <li>
+                <a class="collapsed" data-bs-target="#nav1" data-bs-toggle="collapse">
+                    <i class="bi bi-grid"></i>
+                    Terminologi
+                    <i class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="nav1" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                    <li>
+                      <a href="tables-general.html">
+                        <i class="bi bi-circle"></i><span>General Tables</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="tables-data.html">
+                        <i class="bi bi-circle"></i><span>Data Tables</span>
+                      </a>
+                    </li>
+                  </ul>
+            </li>
+            
+            <li>
+                <a class="collapsed" data-bs-target="#nav2" data-bs-toggle="collapse">
+                    <i class="bi bi-grid"></i>
+                    Pengantar
+                    <i class="bi bi-chevron-down ms-auto"></i>
+                </a>
+                <ul id="nav2" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+                    <li>
+                      <a href="tables-general.html">
+                        <i class="bi bi-circle"></i><span>General Tables</span>
+                      </a>
+                    </li>
+                    <li>
+                      <a href="tables-data.html">
+                        <i class="bi bi-circle"></i><span>Data Tables</span>
+                      </a>
+                    </li>
+                  </ul>
+            </li>
+            
+            <li>
+                <a class="collapsed" href="get-quote.html">
+                    <i class="bi bi-person"></i>
+                    <span>Get Quote</span>
+                </a>
+            </li>
+            
+        </ul>
+    </div><!-- End sidebar categories-->
+    
+</div><!-- End sidebar -->
+
+
+
+
+
+    </nav>
+`;
+document.getElementById("app-sidebar").innerHTML = appSidebar;
---
+++ 0footer.js
@@ -0,0 +1,133 @@
+let appFooter = `
+    <nav>
+
+
+
+
+
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    
+      <title>W&G PRUfesor Asuransi</title>
+      <meta content="" name="description">
+      <meta content="" name="keywords">
+    
+      <!-- Favicons -->
+      <link href="assets/img/favicon.png" rel="icon">
+      <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    
+      <!-- Google Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    
+      <!-- Vendor CSS Files -->
+      <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+      <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+      <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+      <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+      <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    
+      <!-- Template Main CSS File -->
+      <link href="assets/css/style.css" rel="stylesheet">
+    
+    </head>
+    
+    <body>
+    
+      <!-- ======= Footer ======= -->
+      <footer id="footer">
+    
+        <div class="footer-top">
+          <div class="container">
+            <div class="row">
+    
+              <div class="col-lg-4 col-md-3 footer-contact">
+                <h3><span>PRU</span>fesor<br>Asuransi</h3><br>
+                <p>
+                  USTeams Prudential <br>
+                  Mangga Dua Square<br>
+                  Lantai 1 Blok A no. 46-56<br>
+                  Jakarta Utara <br>
+                  14420 <br>
+                </p>
+              </div>
+    
+              <div class="col-lg-4 col-md-3 footer-links">
+                <h4>Useful Links</h4>
+                <ul>
+                  <li><i class="bx bx-chevron-right"></i> <a href="index.html#about">About us</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="index.html#products">Products</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="index.html#services">Services</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="index.html#team">Team</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="index.html#contact">Contact</a></li>
+                </ul>
+              </div>
+    
+              <div class="col-lg-4 col-md-3 footer-links">
+                <h4>Our Services</h4>
+                <ul>
+                  <li><i class="bx bx-chevron-right"></i> <a href="get-quote.html">Dedicated Servicing Agent</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="get-quote.html">Ilustrasi Premi Asuransi</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="get-quote.html">Review Polis Asuransi Lama</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="get-quote.html">QnA Terkait Asuransi</a></li>
+                  <li><i class="bx bx-chevron-right"></i> <a href="get-quote.html">Peluang Bisnis</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+    
+        <div class="container py-4">
+    
+          <div class="me-md-auto text-center text-md-start">
+            <div class="copyright">
+              &copy; Copyright <a href="https://limwilliam.com/"><strong><span>WilliamLim</span></strong></a>
+            </div>
+            <div class="credits">
+              <strong>All Rights Reserved</strong>
+            </div>
+          </div>
+          <!-- (signed-off: WilliamLim)
+          <div class="social-links text-center text-md-end pt-3 pt-md-0">
+            <a href="https://www.instagram.com/limwilliam10/" class="instagram"><i class="bx bxl-instagram"></i></a>
+            <a href="https://www.linkedin.com/in/lim-william/" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+            <a href="https://wa.me/+6281218035252" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+            <a href="mailto:limwilliam.id@gmail.com" class="google-plus"><i class="bx bxl-google"></i></a>
+            <a href="https://www.facebook.com/sherringford/" class="facebook"><i class="bx bxl-facebook"></i></a>
+            <a href="https://twitter.com/LimWilliam_" class="twitter"><i class="bx bxl-twitter"></i></a>
+          -->
+          </div>
+        </div>
+      </footer>
+      <!-- End Footer -->
+    
+      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    
+      <!-- Vendor JS Files -->
+      <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+      <script src="assets/vendor/aos/aos.js"></script>
+      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+      <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+      <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+      <script src="assets/vendor/php-email-form/validate.js"></script>
+    
+      <!-- Template Main JS File -->
+      <script src="assets/js/main.js"></script>
+    
+    </body>
+    
+    </html>
+
+
+
+
+    
+    </nav>
+`;
+document.getElementById("app-footer").innerHTML = appFooter;
