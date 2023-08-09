6c645a6781b182459e5d702100d063ba2c2365ee
limwilliam10 <80199957+limwilliam10@users.noreply.github.com>
2023-08-09 19:03:46 +0700

    change address

--- 0footer.js
+++ 0footer.js
@@ -51,7 +51,7 @@ let appFooter = `
                 <p>
                   USTeams Prudential <br>
                   Mangga Dua Square<br>
-                  Lantai 1 Blok A no. 46-56<br>
+                  Lt. 1 Blok A no. 46-56<br>
                   Jakarta Utara <br>
                   14420 <br>
                 </p>
@@ -105,20 +105,6 @@ let appFooter = `
         </div>
       </footer>
       <!-- End Footer -->
-    
-      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-    
-      <!-- Vendor JS Files -->
-      <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-      <script src="assets/vendor/aos/aos.js"></script>
-      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-      <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-      <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-      <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-      <script src="assets/vendor/php-email-form/validate.js"></script>
-    
-      <!-- Template Main JS File -->
-      <script src="assets/js/main.js"></script>
     
     </body>
     
