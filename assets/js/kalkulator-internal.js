64b61f7eb593d84d17b277c1f7d97bdd5e5698bc
limwilliam10 <80199957+limwilliam10@users.noreply.github.com>
2025-09-25 03:37:49 +0700

    remove password

--- assets/js/kalkulator-internal.js
+++ assets/js/kalkulator-internal.js
@@ -427,24 +427,3 @@ async function populateProductDropdown() {
 
 // Panggil fungsi ini saat seluruh konten halaman HTML sudah siap
 document.addEventListener('DOMContentLoaded', populateProductDropdown);
-
-
-
-
-
-/* Fungsi untuk menampilkan kalkulator jika password benar
-function showCalculator() {
-  kalkulatorContainer.querySelector('#kalkulator-premi-container').style.display = 'block';
-}
-// Fungsi untuk meminta password saat halaman dimuat
-function askPassword() {
-  var password = prompt("Silakan masukkan password untuk mengakses Kalkulator Internal USTeams:");
-  if (password === "usteamsnumber1") {
-    showCalculator();
-  } else {
-    alert("Password salah!");
-    window.location.href = "https://www.prufesorasuransi.com/kalkulator-premi";
-  }
-}
-// Panggil fungsi saat halaman siap
-document.addEventListener('DOMContentLoaded', askPassword);
