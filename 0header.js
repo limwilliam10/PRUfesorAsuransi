9dabd39f9a272b22b6f14403fe36e86adb90a1bf
limwilliam10 <80199957+limwilliam10@users.noreply.github.com>
2025-09-26 14:59:36 +0700

    deployed kalkulator v2

--- 0header.js
+++ 0header.js
@@ -17,23 +17,23 @@ let appHeader = `
       <meta content="" name="keywords">
     
       <!-- Favicons -->
-      <link href="assets/img/favicon.png" rel="icon">
-      <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+      <link href="/assets/img/favicon.png" rel="icon">
+      <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
     
       <!-- Google Fonts -->
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
     
       <!-- Vendor CSS Files -->
-      <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-      <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-      <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-      <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-      <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-      <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-      <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+      <link href="/assets/vendor/aos/aos.css" rel="stylesheet">
+      <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+      <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+      <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+      <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+      <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
     
       <!-- Template Main CSS File -->
-      <link href="assets/css/style.css" rel="stylesheet">
+      <link href="/assets/css/style.css" rel="stylesheet">
     
     </head>
     
@@ -43,7 +43,7 @@ let appHeader = `
       <header id="header" class="fixed-top d-flex align-items-center">
         <div class="container d-flex align-items-center justify-content-between position-relative">
     
-          <a href="https://www.prufesorasuransi.com" class="logo me-3"><img src="assets/img/logo.png"></a>
+          <a href="https://www.prufesorasuransi.com" class="logo me-3"><img src="/assets/img/logo.png"></a>
           <h1 class="logo me-auto"><a href="https://www.prufesorasuransi.com"><span>PRU</span>fesor<br>Asuransi</a></h1>
           <nav id="navbar" class="navbar order-last order-lg-0">
             <ul>
@@ -139,16 +139,16 @@ let appHeader = `
       <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
     
       <!-- Vendor JS Files -->
-      <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-      <script src="assets/vendor/aos/aos.js"></script>
-      <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-      <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
-      <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-      <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-      <script src="assets/vendor/php-email-form/validate.js"></script>
+      <script src="/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+      <script src="/assets/vendor/aos/aos.js"></script>
+      <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+      <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+      <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+      <script src="/assets/vendor/php-email-form/validate.js"></script>
     
       <!-- Template Main JS File -->
-      <script src="assets/js/main.js"></script>
+      <script src="/assets/js/main.js"></script>
     
     </body>
     
