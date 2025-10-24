0f2b1f9bd7f1206e37da2b63c505937fe129c7d0
limwilliam10 <80199957+limwilliam10@users.noreply.github.com>
2025-10-25 03:56:40 +0700

    Update 0header.js

--- 0header.js
+++ 0header.js
@@ -119,6 +119,7 @@ let appHeader = `
               
               <li class="dropdown"><a href="https://www.prufesorasuransi.com#products"><span>Products</span> <i class="bi bi-chevron-down"></i></a>
                 <ul>
+                  <li><a href="/kalkulator-premi-kesehatan">Kalkulator Premi Asuransi Kesehatan</a></li>
                   <li><a href="/kalkulator-premi">Kalkulator Premi Asuransi Jiwa</a></li>
                 </ul>
               </li>
