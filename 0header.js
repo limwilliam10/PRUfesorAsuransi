ca97bde9270e4a105f4d9763e7f0677ee42537b6
limwilliam10 <80199957+limwilliam10@users.noreply.github.com>
2025-10-25 04:05:12 +0700

    Create 0header.js

---
+++ 0header.js
@@ -0,0 +1,164 @@
+let appHeader = `
+    <nav>
+
+
+
+    
+
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    
+      <title>W&G PRUfesor Asuransi</title>
+      <meta content="" name="description">
+      <meta content="" name="keywords">
+    
+      <!-- Favicons -->
+      <link href="/assets/img/favicon.png" rel="icon">
+      <link href="/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+    
+      <!-- Google Fonts -->
+      <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    
+      <!-- Vendor CSS Files -->
+      <link href="/assets/vendor/aos/aos.css" rel="stylesheet">
+      <link href="/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+      <link href="/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+      <link href="/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+      <link href="/assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+      <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+      <link href="/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    
+      <!-- Template Main CSS File -->
+      <link href="/assets/css/style.css" rel="stylesheet">
+    
+    </head>
+    
+    <body>
+    
+      <!-- ======= Header ======= -->
+      <header id="header" class="fixed-top d-flex align-items-center">
+        <div class="container d-flex align-items-center justify-content-between position-relative">
+    
+          <a href="https://www.prufesorasuransi.com" class="logo me-3"><img src="/assets/img/logo.png"></a>
+          <h1 class="logo me-auto"><a href="https://www.prufesorasuransi.com"><span>PRU</span>fesor<br>Asuransi</a></h1>
+          <nav id="navbar" class="navbar order-last order-lg-0">
+            <ul>
+              
+              <!-- (signed-off: WilliamLim)
+              <li><a class="nav-link scrollto active" href="https://www.prufesorasuransi.com#hero">Home</a></li>
+    
+              <li><a class="nav-link scrollto" href="https://www.prufesorasuransi.com#portfolio">Portfolio</a></li>
+              
+              <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                <ul>
+                  <li><a href="#">Drop Down 1</a></li>
+                  <li><a href="#">Drop Down 2</a></li>
+                  <li><a href="#">Drop Down 3</a></li>
+                  <li><a href="#">Drop Down 4</a></li>
+                  <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                    <ul>
+                      <li><a href="#">Deep Drop Down 1 Deep Drop Down 1</a></li>
+                      <li><a href="#">Deep Drop Down 2</a></li>
+                      <li><a href="#">Deep Drop Down 3</a></li>
+                      <li><a href="#">Deep Drop Down 4</a></li>
+                      <li><a href="#">Deep Drop Down 5</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+    
+              <li class="dropdown megamenu"><a href="#"><span>Mega Menu</span> <i class="bi bi-chevron-down"></i></a>
+                <ul>
+                  <li>
+                    <strong>Column 1</strong>
+                    <a href="#">Column 1 link 1 Column 1 link 1 Column 1 link 1</a>
+                    <a href="#">Column 1 link 2</a>
+                    <a href="#">Column 1 link 3</a>
+                    <div class="dropdown"><a href="#"><span>Column 1 link 4</span> <i class="bi bi-chevron-right position-relative"></i></a>
+                      <ul>
+                        <li>
+                          <a href="#">Column 1 link 4.1 Column 1 link 4.1 Column 1 link 4.1</a>
+                          <a href="#">Column 1 link 4.2</a>
+                          <a href="#">Column 1 link 4.3</a>
+                        </li>
+                      </ul>
+                    </div>
+                  <li>
+                    <strong>Column 2</strong>
+                    <a href="#">Column 2 link 1</a>
+                    <a href="#">Column 2 link 2</a>
+                    <a href="#">Column 3 link 3</a>
+                  </li>
+                  <li>
+                    <strong>Column 3</strong>
+                    <a href="#">Column 3 link 1</a>
+                    <a href="#">Column 3 link 2</a>
+                    <a href="#">Column 3 link 3</a>
+                  </li>
+                  <li>
+                    <strong>Column 4</strong>
+                    <a href="#">Column 4 link 1</a>
+                    <a href="#">Column 4 link 2</a>
+                    <a href="#">Column 4 link 3</a>
+                  </li>
+                  <li>
+                    <strong>Column 5</strong>
+                    <a href="#">Column 5 link 1</a>
+                    <a href="#">Column 5 link 2</a>
+                    <a href="#">Column 5 link 3</a>
+                  </li>
+                </ul>
+              </li>
+              -->
+    
+              <li><a href="https://www.prufesorasuransi.com#about">About</a></li>
+              
+              <li class="dropdown"><a href="https://www.prufesorasuransi.com#products"><span>Products</span> <i class="bi bi-chevron-down"></i></a>
+                <ul>
+                  <li><a href="/kalkulator-premi-kesehatan">Kalkulator Premi Asuransi Kesehatan</a></li>
+                  <li><a href="/kalkulator-premi">Kalkulator Premi Asuransi Jiwa</a></li>
+                </ul>
+              </li>
+              
+              <li><a href="https://www.prufesorasuransi.com#services">Services</a></li>
+              <li><a href="https://www.prufesorasuransi.com#team">Team</a></li>
+              <li><a href="/articles">Articles</a></li>
+              <li><a href="https://www.prufesorasuransi.com#contact">Contact</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav><!-- .navbar -->
+    
+          <a href="/get-quote" class="get-started-btn scrollto">Get Quote</a>
+        </div>
+      </header>
+      <!-- End Header -->
+    
+      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    
+      <!-- Vendor JS Files -->
+      <script src="/assets/vendor/purecounter/purecounter_vanilla.js"></script>
+      <script src="/assets/vendor/aos/aos.js"></script>
+      <script src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+      <script src="/assets/vendor/glightbox/js/glightbox.min.js"></script>
+      <script src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+      <script src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+      <script src="/assets/vendor/php-email-form/validate.js"></script>
+    
+      <!-- Template Main JS File -->
+      <script src="/assets/js/main.js"></script>
+    
+    </body>
+    
+    </html>
+
+
+
+
+
+    </nav>
+`;
+document.getElementById("app-header").innerHTML = appHeader;
